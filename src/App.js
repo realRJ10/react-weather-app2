051fd66923ceaf80a2749f0d8f68d7b1680a3526
realRJ10 <121657363+realRJ10@users.noreply.github.com>
2024-04-05 16:08:56 +0330

    added bootstrap and weather

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/realRJ10/react-weather-app2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          This project
-        </a>{" "}
-        is coded by Mojdeh Khorsand Moghadam
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          <a
+            href="https://github.com/realRJ10/react-weather-app2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            This project
+          </a>{" "}
+          is coded by Mojdeh Khorsand Moghadam
+        </footer>
+      </div>
     </div>
   );
 }
